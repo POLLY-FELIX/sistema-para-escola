323bb83d1bb48a40e3e7d641abd84402c944a3e8
Perola <perola.polly.felix@gmail.com>
2024-02-01 21:10:55 -0300

    alteração

--- Program.cs
+++ Program.cs
@@ -12,49 +12,58 @@ namespace sistema_para_escola
         static void Main(string[] args)
         {
             int menu = 0;
+            string[] aluno = new string[2];
+            int[] idade = new int[2];
+            string[] sexo = new string[2];
 
             while (menu != 3)
             {
+
                 if (menu == 0)
                 {
                     Console.WriteLine("1 - Adicionar alunos");
                     Console.WriteLine("2 - lista de alunos");
                     Console.WriteLine("3 - sair");
+                    int.TryParse(Console.ReadLine(), out menu);
                 }
-                int.TryParse(Console.ReadLine(), out menu);
+
 
                 if (menu == 1)
                 {
-                    string[] alunos = new string[2];
-                    int[] idade = new int[2];
-                    string[] sexo = new string[20];
 
                     //Solicitar o nome dos alunos
-                    for (int index = 0; index < alunos.Length; index++)
+                    for (int index = 0; index < aluno.Length; index++)
                     {
                         Console.WriteLine("Qual nome do aluno?");
-                        Console.ReadLine();
+                        string nome = Console.ReadLine();
 
                         //Solicitar a idade
-
                         Console.WriteLine("Qual a idade?");
-                        Console.ReadLine();
+                        string idades = Console.ReadLine();
 
                         //Solicitar o sexo
-
                         Console.WriteLine("Qual o sexo do aluno(a)?");
-                        Console.ReadLine();
+                        string sex = Console.ReadLine();
 
                         Console.Clear();
+                        aluno[index] = nome;    
                     }
-                    
-                    if (menu == 2)
+                    menu = 0;
+                }
+                else if (menu == 2)
+                {
+
+                    Console.WriteLine("Essa é a lista de alunos");
+                    for(int index = 0; index < aluno.Length; index++)
                     {
-
+                        Console.WriteLine($"{index + 1} - {aluno[index]}");
+                        int.TryParse(Console.ReadLine()), out menu);
                     }
+                }
+                        
                     
 
-                }  
+                
             }
         }
     }
